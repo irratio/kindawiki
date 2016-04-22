a6046895ae16e04ea7ca0a5e18221e29cb1db632
Andrey Shmelkov <irratio@gmail.com>
2016-04-22 14:09:43 +0500

    route constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  scope format: false do
+  scope format: false, constraints: {path: /[a-zA-Z0-9_\/]*/} do
     get    '/(*path)/add',  to: 'pages#new',    as: :new_page
     get    '/(*path)/edit', to: 'pages#edit',   as: :edit_page
     get    '/(*path)',      to: 'pages#show',   as: :page
